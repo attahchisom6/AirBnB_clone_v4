cd839659969a61193d7c6844f35c6f9b6c1663d7
Mondliwethu Vundla <70095612+mondlivundla@users.noreply.github.com>
2023-05-07 12:41:55 +0000

    Update 1-hbnb

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -12,6 +12,9 @@ import uuid
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
+app.url_map.strict_slashes = False
+port = 5000
+host = '0.0.0.0'
 
 
 @app.teardown_appcontext
@@ -45,4 +48,4 @@ def hbnb():
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host=host, port=port)
