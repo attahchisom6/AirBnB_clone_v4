52a3cc664f559ba2e49beae673d38687926bae28
attahchisom6 <attahchisom6@gmail.com>
2023-05-08 16:15:01 +0100

    commit

--- work_on_this/web_dynamic/static/scripts/100-hbnb.js
+++ work_on_this/web_dynamic/static/scripts/100-hbnb.js
@@ -1,8 +1,8 @@
 const request = require('request');
 const url = 'http://0.0.0.0:5001/api/v1/status';
 
-try {
-  $(document).ready(function () {
+$(document).ready(function () {
+  try {
     request.get(url, (error, response) => {
       if (error) {
         console.log(error);
@@ -15,42 +15,50 @@ try {
       }
     });
 
-    let AmenityDict = {};
-    let stateDict = {};
-    let cityDict = {};
-    const dataName = $(this).attr('data-name');
-    const = $(this).attr('data-id');
+    const AmenityDict = {};
+    const stateDict = {};
+    const cityDict = {};
 
-    if $(this).is(':checked') {
-      if $(this).hasClass('amenities') {
-        AmenityDict[dataId] = dataName;
+    $('input[type="checkbox"]').change(function () {
+      const dataId = $(this).attr('data-id');
+      const dataName = $(this).attr('data-name');
+
+      if ($(this).is(':checked')) {
+        if ($(this).hasClass('amenities')) {
+          AmenityDict[dataId] = dataName;
+        } else if ($(this).hasClass('states')) {
+          stateDict[dataId] = dataName;
+        } else if ($(this).hasClass('cities')) {
+          cityDict[dataId] = dataName;
+        }
       } else {
-        delete AmenityDict[dataId];
-      }
-      if $(this).hasClass('states') {
-        stateDict[dataId] = dataName;
-      } else {
-        delete stateDict[dataId];
+        if ($(this).hasClass('amenities')) {
+          delete AmenityDict[dataId];
+        } else if ($(this).hasClass('states')) {
+          delete stateDict[dataId];
+        } else if ($(this).hasClass('cities')) {
+          delete cityDict[dataId];
+        }
       }
 
-      if $(this).hasClass('cities') {
-        cityDict[dataId] = dataName;
-      } else {
-        cityDict[dataId] = dataName;
-      }
       const amenityList = Object.values(AmenityDict).join(', ');
-      const stateDict = Object.values(stateDict).join('. ');
-      const cityDict = Object.values(cityDict).join(', ');
+      const stateList = Object.values(stateDict).join('. ');
+      const cityList = Object.values(cityDict).join(', ');
+
       $('div.amenities h4').text(amenityList);
-      $('locations h4').text(`state: ${stateList}<br>cities: ${cities}`);
+      $('div.locations h4').html(`States: ${stateList}<br>Cities: ${cityList}`);
     });
 
     $('button').click(function () {
       const amenityKeys = Object.keys(AmenityDict);
       const cityKeys = Object.keys(cityDict);
       const stateKeys = Object.keys(stateDict);
-      const data = JSON.stringify({ 'amenities': amenityKeys, 'states': stateKeys, 'cities': cityKeys });
-    }
+
+      const data = JSON.stringify({
+        amenities: amenityKeys,
+        states: stateKeys,
+        cities: cityKeys
+      });
 
       $.ajax({
         url: 'http://0.0.0.0:5001/api/v1/places_search',
@@ -61,32 +69,32 @@ try {
         success: function (response) {
           $('section.places').empty().append(response.map(place => {
             return `<article>
-                    <div class="title_box">
-                      <h2>${place.name}</h2>
-                      <div class="price_by_night">
-                        ${place.price_by_night}
-                      </div>
-                    </div>
-                    <div class="information">
-                      <div class="max_guest">
-                        ${place.max_guest} Guest{% if place.max_guest != 1 %}s{% endif %}
-                      </div>
-                    <div class="number_rooms">
-                      ${place.number_rooms} Bedroom{% if place.number_rooms != 1 %}s{% endif %}
-                    </div>
-                    <div class="number_bathrooms">
-                      ${place.number_bathrooms} Bathroom{% if place.number_bathrooms != 1 %}s{% endif %}
-                    </div>
-                  </div>
-                  <div class="description">
-                    ${place.description | safe}
-                  </div>
-                </article>`;
+              <div class="title_box">
+                <h2>${place.name}</h2>
+                <div class="price_by_night">
+                  ${place.price_by_night}
+                </div>
+              </div>
+              <div class="information">
+                <div class="max_guest">
+                  ${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}
+                </div>
+                <div class="number_rooms">
+                  ${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}
+                </div>
+                <div class="number_bathrooms">
+                  ${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}
+                </div>
+              </div>
+              <div class="description">
+                ${place.description}
+              </div>
+            </article>`;
           }));
         }
       });
     });
-  });
-} catch (error) {
-  console.error(error);
-}
+  } catch (error) {
+    console.error(error);
+  }
+});
