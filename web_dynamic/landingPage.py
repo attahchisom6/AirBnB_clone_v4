68f525c314007211057a2c467f6532ab12f72216
attahchisom6 <attahchisom6@gmail.com>
2023-06-08 15:51:24 +0100

    adding

--- web_dynamic/landingPage.py
+++ web_dynamic/landingPage.py
@@ -4,5 +4,18 @@
 this is a simple web application intended to render a landing page
 """
 from flask import Flask
+from flask import render_template
 
-app = Flask('__name__)
+
+app = Flask('__name__')
+port=5004
+host=localhost
+
+app.route('/MoseLandingPage', strict_slashes=False)
+def landingPage():
+    """our landing page"""
+    return render_template('0-landingPage.html')
+
+
+if __name__ == '__main__':
+    app.run(host=host, port=port)
